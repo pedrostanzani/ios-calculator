3e3ce8d5889bece8f60a9dd948192f376dc0c57e
Pedro Stanzani <pedrohstanzani@gmail.com>
2022-06-15 08:43:52 -0300

    Implement core func. state, selectOperation() and handleEqual() logic

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,19 +4,76 @@ class Calculator extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            firstOperand: 3,
+            secondOperand: 2,
+            operator: null,
+
             display: '0'
         };
     }
 
+    selectOperation(operator) {
+        this.setState({ operator });
+    }
+
+    handleEqual() {
+        let first, second, result;
+
+        first = this.state.firstOperand;
+        second = this.state.secondOperand;
+
+        console.log(first, second);
+
+        switch (this.state.operator) {
+            case '+': result = first + second; break;
+            case '-': result = first - second; break;
+            case 'x': result = first * second; break;
+            case '÷': result = first / second; break;
+        
+            default:
+                break;
+        }
+
+        if (!isNaN(result)) {
+            this.setState({firstOperand: result});
+        }
+
+        console.log(result);
+    }
+
     render() {
         const digits = [...Array(10).keys()].map((i) => {
-            return (<button>{i}</button>);
+            return (<button key={i}>{i}</button>);
         });
 
         return (
             <div>
                 <div className='display'>{this.state.display}</div>
-                {digits}
+
+                <div>
+                    {digits}
+                    <button>.</button>
+                </div>
+
+                <div>
+                    <button>AC</button>
+                    <button>+/-</button>
+                    <button>%</button>
+                </div>
+
+                <div>
+                    <button onClick={() => this.selectOperation('÷')}>÷</button>
+                    <button onClick={() => this.selectOperation('x')}>x</button>
+                    <button onClick={() => this.selectOperation('+')}>+</button>
+                    <button onClick={() => this.selectOperation('-')}>-</button>
+
+                    <button onClick={() => this.handleEqual()}>=</button>
+                </div>
+
+
+
+
+
             </div>
         );
     }
